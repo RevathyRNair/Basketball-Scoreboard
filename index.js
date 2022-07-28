89b3b58deb183364a09eeee578177265150a172a
Revathy Nair <revu.rajasree@gmail.com>
2022-07-28 18:36:43 +0400

    changes made as per review comments

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let rounds = 1;
+let homeTotalScore = 0;
+let guestTotalScore = 0;
+let matchDuration = 12;
+
 const startButton = document.getElementById("start-btn");
 const minuteVal = document.querySelector(".minute-val");
 const secondVal = document.querySelector(".seconds-val");
@@ -7,10 +12,11 @@ const homeRound2Score = document.querySelector(".home-round-2");
 const homeRound3Score = document.querySelector(".home-round-3");
 const homeRound4Score = document.querySelector(".home-round-4");
 
-const homePoint = document.querySelector(".home-point");
-const guestPoint = document.querySelector(".guest-point");
+const homePoint = document.querySelector(".home-pt");
+const guestPoint = document.querySelector(".guest-pt");
 
 const addPointButton = document.querySelectorAll(".add-pt-btn");
+console.log(addPointButton);
 
 const guestRound1Score = document.querySelector(".guest-round-1");
 const guestRound2Score = document.querySelector(".guest-round-2");
@@ -19,115 +25,92 @@ const guestRound4Score = document.querySelector(".guest-round-4");
 
 const message = document.querySelector(".display-msg");
 
-let rounds = 1;
-let hval = 0;
-let gval = 0;
-let matchDuration = 0.25;
+const renderMins = (minVal) => (minuteVal.textContent = String(minVal).padStart(2, 0));
 
-function addHomePt1() {
-    hval = hval + 1;
-    console.log(hval);
-    document.getElementById("home-pt").textContent = hval;
-}
+const renderSecs = (secValue) => (secondVal.textContent = String(secValue).padStart(2, 0));
 
-function addHomePt2() {
-    hval = hval + 2;
-    console.log(hval);
-    document.getElementById("home-pt").textContent = hval;
-}    
+const addHomeRound1Score = (score) => (homeRound1Score.textContent = score);
+const addGuestRound1Score = (score) => (guestRound1Score.textContent = score);
 
-function addHomePt3() {
-    hval = hval + 3;
-    console.log(hval);
-    document.getElementById("home-pt").textContent = hval;
-}
+const addHomeTotalScore = (val) => (homeTotalScore += val);
+const displayHomeScore = (score) => (homePoint.textContent = score);
+const addGuestTotalScore = (val) => (guestTotalScore += val);
+const displayGuestScore = (score) => (guestPoint.textContent = score);
 
-function addGuestPt1() {
-    gval = gval + 1;
-    console.log(gval);
-    document.getElementById("guest-pt").textContent = gval;
-}
+const showMessage = (msgEl, msgVal) => (msgEl.textContent = msgVal);
 
-function addGuestPt2() {
-    gval = gval + 2;
-    console.log(gval);
-    document.getElementById("guest-pt").textContent = gval;
-}
+addPointButton.forEach((btn) => {
+    console.log("Entered forEach loop");
+    btn.addEventListener("click", function (e) {
+        console.log("Entered click event");
+        const buttonIsWithAttribute = e.target.getAttribute("data-addPointAttribute");
+        console.log(buttonIsWithAttribute);
+    
+        if (buttonIsWithAttribute === "home-add-1") {
+            addHomeTotalScore(1);
+            displayHomeScore(homeTotalScore);
+        }
+    
+        if (buttonIsWithAttribute === "home-add-2") {
+            addHomeTotalScore(2);
+            displayHomeScore(homeTotalScore);
+        }
+    
+        if (buttonIsWithAttribute === "home-add-3") {
+            addHomeTotalScore(3);
+            displayHomeScore(homeTotalScore);
+        }
+    
+        if (buttonIsWithAttribute === "guest-add-1") {
+            addGuestTotalScore(1);
+            displayGuestScore(guestTotalScore);
+        }
+    
+        if (buttonIsWithAttribute === "guest-add-2") {
+            addGuestTotalScore(2);
+            displayGuestScore(guestTotalScore);
+        }
+    
+        if (buttonIsWithAttribute === "guest-add-3") {
+            addGuestTotalScore(3);
+            displayGuestScore(guestTotalScore);
+        }
+        });
+});
+ 
 
-function addGuestPt3() {
-    gval = gval + 3;
-    console.log(gval);
-    document.getElementById("guest-pt").textContent = gval;
-}
-
-function reset() {
-    hval = 0;
-    gval = 0;
-    document.getElementById("home-pt").textContent = hval;
-    document.getElementById("guest-pt").textContent = gval;
-}
-
-//JS for timer
-
-let interval = null;
-const startingMinutes = 12;
-let time = startingMinutes * 60;
-const countdownEl = document.getElementById("time-val");
+startButton.addEventListener("click", start);
 
 function start() {
-    if(interval) {
-        return
+    console.log("Start button clicked");
+    let mins, sec, timeup;
+    let interval;
+    let timer = matchDuration * 60;
+    function startTimer() {
+        mins = Math.floor(timer / 60);
+        sec = timer % 60;
+        timeup = mins <= 0 && sec <= 0;
+    
+        renderMins(mins);
+        renderSecs(sec);
+    
+        if(timeup) {
+            clearInterval(interval);
+            addHomeRound1Score(homePoint.textContent);
+            addGuestRound1Score(guestPoint.textContent);
+            showMessage(message, "Round one Over");
+            resetScores();
+        }
+    
+        timer --;
     }
-
-    interval = setInterval(updateCountdown, 1000);
+    interval = setInterval(startTimer, 1000);
 }
 
-function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    countdownEl.textContent = minutes + ":" + seconds;
-    console.log(countdownEl.textContent);
-
-    if(minutes<0){ 
-        showMsg();
-        reset();
-        setTimeout(resetWindow, 5000);
-        if(rounds == 1) {
-            document.getElementById("h-val1").textContent = hval;
-            document.getElementById("g-val1").textContent = gval;
-        }
-        if(rounds == 2) {
-            document.getElementById("h-val2").textContent = hval;
-            document.getElementById("g-val2").textContent = gval;
-        }
-        if(rounds == 3) {
-            document.getElementById("h-val3").textContent = hval;
-            document.getElementById("g-val3").textContent = gval;
-        }
-        if(rounds == 4) {
-            document.getElementById("h-val4").textContent = hval;
-            document.getElementById("g-val4").textContent = gval;
-        }
-        
-    }
-    time--;
-    console.log(time);
-}
-
-function resetWindow() {
-    window.location.href="index.html";
-}
-
-function showMsg() {
-    let matchOverMsg = document.getElementById("match-over");
-    matchOverMsg.textContent = "Round " + rounds + " Over!";
-    console.log(matchOverMsg.textContent);
-}
-
-function reset() {
-    document.getElementById("home-pt").textContent = 0;
-    document.getElementById("guest-pt").textContent = 0;
-    document.getElementById("time-val").textContent = "12:00";
-}
-
+const resetScores = () => {
+    setTimeout(() => {
+      showMessage(message, " ");
+      homePoint.textContent = guestPoint.textContent = 0;
+    }, 5000);
+};
 
